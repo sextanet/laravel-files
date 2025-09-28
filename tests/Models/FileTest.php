8e3eef5f5d6d7ae50bb233f015b82b3f1b1d82b0
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2025-09-28 14:04:41 -0300

    fix: all pest erorrs

--- tests/Models/FileTest.php
+++ tests/Models/FileTest.php
@@ -15,28 +15,28 @@ beforeEach(function () {
 });
 
 it('can get the path', function () {
-    expect($this->file->getPath())
+    expect($this->file->path())
         ->toBe(Storage::disk('local')->path($this->file->path));
 });
 
 it('can get the url', function () {
-    expect($this->file->getUrl())
+    expect($this->file->url())
         ->toBe(Storage::disk('local')->url($this->file->path));
 });
 
 it('can get the temporary url with specific minutes', function () {
-    expect($this->file->getTemporaryUrl(20))
+    expect($this->file->temporaryUrl(20))
         ->toBe(Storage::disk('local')->temporaryUrl($this->file->path, now()->addMinutes(20)));
 });
 
 it('can get the temporary url with default minutes', function () {
-    expect($this->file->getTemporaryUrl())
+    expect($this->file->temporaryUrl())
         ->toBe(Storage::disk('local')->temporaryUrl($this->file->path, now()->addMinutes(5)));
 });
 
 it('can get the temporary url with default forced minutes', function () {
     config(['files.temporary_url_minutes' => 10]);
 
-    expect($this->file->getTemporaryUrl())
+    expect($this->file->temporaryUrl())
         ->toBe(Storage::disk('local')->temporaryUrl($this->file->path, now()->addMinutes(10)));
 });
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,15 @@
 <?php
 
+use SextaNet\LaravelFiles\Models\YourModel;
 use SextaNet\LaravelFiles\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
 
 function fake_model()
 {
-    return new SextaNet\LaravelFiles\Models\YourModel;
+    $model = new YourModel;
+
+    $model->id = 1;
+
+    return $model;
 }
