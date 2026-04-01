eaa751b9500dae768c39e7462968d62d3d529ea8
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2026-04-01 15:20:54 -0300

    chore(fix-tests): move abstract class to class

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -4,7 +4,7 @@ namespace SextaNet\LaravelFiles\Models;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
 
-abstract class Model extends BaseModel
+class Model extends BaseModel
 {
     protected $guarded = [];
 }
